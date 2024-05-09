98a00b790f429dddcad1e299b7418d938e2de214
KaziSamiUllah <samiullahsagor@gmail.com>
2024-05-10 00:49:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Simple crud is running");
+});
+
+app.listen(port, () => {
+  console.log(`crud is running through ${port}`);
+});
+
+const uri =
+  "mongodb+srv://samiullahsagor:zfHtJFucCrmqV2gx@cluster0.zunyezi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const usersDB = client.db("ReadopiaDB").collection("Users");    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
