b309fa206a4203ee07ad0d5e965e5a574f74cf9e
KaziSamiUllah <samiullahsagor@gmail.com>
2024-05-14 00:28:06 +0600

    added role features

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+
+const vrifyToken = (req, res, next) => {
+  console.log('ami middle')
+
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: 'unauthorized access' })
+  if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+          if (err) {
+              console.log(err)
+             return res.status(401).send({ message: 'unauthorized access' })
+          }
+          console.log(decode);
+          req.user = decode
+          next();
+      })
+  }
+  console.log(token);
+
+
+}
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Simple crud is running");
@@ -17,8 +53,7 @@ app.listen(port, () => {
   console.log(`crud is running through ${port}`);
 });
 
-const uri =
-  "mongodb+srv://samiullahsagor:zfHtJFucCrmqV2gx@cluster0.zunyezi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zunyezi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,57 +66,74 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    //////////////////// User APIs/////////////////////////////
-    const usersDB = client.db("readopiaDB").collection("users");
 
-    app.post("/users", async (req, res) => {
-      const users = req.body;
-      console.log(users);
-      const result = await usersDB.insertOne(users);
-      res.send(result);
+
+    /////////////////////////////JWT Auth ///////////////////////
+
+    // JWT Generate
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { userEmail: email };
-      const userData = await usersDB.findOne(query);
-      res.send(userData);
+    //clearing Token
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
     });
 
 
-    //////////////////// Book Category info APIs/////////////////////////////
-    const categoryCollection = client.db("readopiaDB").collection("categories");
-    app.post("/categories", async (req, res) => {
-      const category = req.body;
-      // console.log(category);
-      const result = await categoryCollection.insertOne(category);
-      res.send(result);
-    });
+        //////////////////// User APIs/////////////////////////////
+        const usersDB = client.db("readopiaDB").collection("users");
 
-    app.get("/categories", async (req, res) => {
-      const data = categoryCollection.find();
-      const result = await data.toArray();
-      res.send(result);
-    });
+        app.post("/users", async (req, res) => {
+          const users = req.body;
+          console.log(users);
+          const result = await usersDB.insertOne(users);
+          res.send(result);
+        });
+    
+        app.get("/users/:email", vrifyToken, async (req, res) => {
+          const email = req.params.email;
+          const query = { userEmail: email };
+          const userData = await usersDB.findOne(query);
+          res.send(userData);
+        });
+
 
     ////////////////////book Data APIs/////////////////////////
 
     const bookCollection = client.db("readopiaDB").collection("books");
 
-    app.get("/books", async (req, res) => {
+    app.get("/books", vrifyToken, async (req, res) => {
       const data = bookCollection.find();
       const result = await data.toArray();
       res.send(result);
     });
 
-    app.post("/books", async (req, res) => {
+    app.post("/books", vrifyToken, async (req, res) => {
       const book = req.body;
       // console.log(book);
       const result = await bookCollection.insertOne(book);
       res.send(result);
     });
 
-    app.get("/books/:name", async (req, res) => {
+    app.get("/books/:name", vrifyToken,  async (req, res) => {
       const name = req.params.name;
       const query = { name: name };
       const book = await bookCollection.findOne(query);
@@ -90,7 +142,7 @@ async function run() {
 
     ////////////Updating book quantity////////////////////
 
-    app.put("/books/:id", async (req, res) => {
+    app.put("/books/:id", vrifyToken,  async (req, res) => {
       const data = req.body;
       console.log(data);
       const paramsId = req.params.id;
@@ -111,19 +163,16 @@ async function run() {
           options
         );
         res.send(result);
-      }
-
-
-       else {
+      } else {
         const updateDoc = {
           $set: {
-            url : data.url,
-            name : data.name,
-            quantity : data.quantity,
-            author : data.author,
-            category : data.category,
-            description : data.description,
-            rating : data.rating
+            url: data.url,
+            name: data.name,
+            quantity: data.quantity,
+            author: data.author,
+            category: data.category,
+            description: data.description,
+            rating: data.rating,
           },
         };
         const result = await bookCollection.updateOne(
@@ -135,11 +184,26 @@ async function run() {
       }
     });
 
-
-
     ///////////////Update book data/////////////////////
 
     /////////////////Category APIs///////////////////////
+
+    
+    const categoryCollection = client.db("readopiaDB").collection("categories");
+    app.post("/categories", async (req, res) => {
+      const category = req.body;
+      // console.log(category);
+      const result = await categoryCollection.insertOne(category);
+      res.send(result);
+    });
+
+    app.get("/categories",  async (req, res) => {
+      const data = categoryCollection.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+
 
     app.get("/categories/:name", async (req, res) => {
       const categoryName = req.params.name;
@@ -153,20 +217,20 @@ async function run() {
     //////////borrowedBooks////////////
     const borrowdBooks = client.db("readopiaDB").collection("borrowed");
 
-    app.post("/borrowed", async (req, res) => {
+    app.post("/borrowed",  async (req, res) => {
       const borrowed = req.body;
       const result = await borrowdBooks.insertOne(borrowed);
       res.send(result);
     });
 
-    app.get("/borrowed/:email", async (req, res) => {
+    app.get("/borrowed/:email", vrifyToken,  async (req, res) => {
       const userEmail = req.params.email;
       console.log(userEmail);
       const result = await borrowdBooks.find({ email: userEmail }).toArray();
       res.send(result);
     });
 
-    app.delete("/borrowed/:id", async (req, res) => {
+    app.delete("/borrowed/:id", vrifyToken,  async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
